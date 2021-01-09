4ad256d23f7a19bbae116130d1cf58bb42aa0523
Wassim Khleyfi <wassim.khleyfi@gmail.com>
2021-01-09 20:40:17 +0100

    ajout du checkpoint react jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import imageInSrc from './imageInSrc.jpg';
+import './style.css';
 import './App.css';
+import felicitation from "./myVideo.mp4";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       <div className ="first" style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+       <h1 className="title red">Wassim Khleyfi</h1>
+       <br />
+       <h2 className="infoimg1">This is an image in src:</h2>
+       <img className ="shadowAndBorder" src={imageInSrc} alt="imageInSrc" />  
+       <br />
+       <h2 className="infoimg2">This is an image in public:</h2>
+       <img className ="shadowAndBorder" width={700} height={572} src="/images/imageInPublic.jpg" alt="imageInPublic" />
+ 
+      </div>
+      <br />
+     <video className ="shadowAndBorder"style= {{padding : "50px"}} src={felicitation} width={320} height={240} controls="controls" autoplay="true">
+     </video> 
+  
+  </div>
   );
 }
 
 export default App;
+       
+     
+ 
